e43d6c656620c2bc69ac2092fc4c3fc5215a5988
Alexandre Machado <117436159+LXMachado@users.noreply.github.com>
2025-09-19 18:38:39 +1000

    Fix App test import and expectation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import App from "./app/App";
+import store from "./app/store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders app header", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(screen.getByRole("heading", { name: /Reddish/i })).toBeInTheDocument();
 });
